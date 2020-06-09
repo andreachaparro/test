5f5b7e6f74ed95ea8911aabe70368d85ac2078ff
andreachaparro <anstchamon0519@hotmail.com>
2020-06-09 18:11:53 -0500

    version 1

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationLogin.Models;
+using System.Data.SqlClient;
+
+
+namespace WebApplicationLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account1
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source= LAPTOP-9B3QJ4SS; database=DemoLogin; integrated security=SSPI;";
+        }
+        [HttpPost]
+        public ActionResult verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where username = '" + acc.Name + "' and password = '" + acc.Password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult addCount(Account acc)
+        {
+            //Account acc = new Account();
+        
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            acc.Name = Request.Form["username"].ToString();
+            acc.edad = Request.Form["edad"].ToString();
+            acc.telephone = Request.Form["telephone"].ToString();
+            acc.email = Request.Form["email"].ToString();
+            acc.Password = Request.Form["apellido"].ToString();
+            com.CommandText = "insert into tbl_login values('" + acc.Name + "';'" + acc.edad + "';'" + acc.telephone + "';'" + acc.email + "';'" + acc.Password + "')";
+            dr = com.ExecuteReader();
+                con.Close();
+                return View("Create");
+   
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationLogin.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string edad { get; set; }
+        public string telephone { get; set; }
+        public string email { get; set; }
+
+    }
+}
